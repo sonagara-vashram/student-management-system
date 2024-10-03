2eeecbbdbfe1b25539cac87a261320d791b09920
sonagara-vashram <vashramsonagara0106@gmail.com>
2024-10-03 10:59:57 +0530

    updated main file

--- app/main.py
+++ app/main.py
@@ -7,7 +7,6 @@ app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 # models.Base.metadata.drop_all(bind=engine)
 
-
 if __name__ == '__main__':
     import uvicorn
     uvicorn.run(app, host='127.0.0.1', port=8000)
