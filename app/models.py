110068b9c71cb5bb07926a14059aab999315bd61
sonagara-vashram <vashramsonagara0106@gmail.com>
2024-10-06 22:05:29 +0530

    models user file

--- app/models.py
+++ app/models.py
@@ -13,7 +13,7 @@ class RoleEnum(enum.Enum):
     TEACHER = "teacher"
     PARENT = "parent"
 
-# Admin table - Main table to manage other roles
+# Admins table - Manages roles of users
 class Admins(Base):
     __tablename__ = "admins"
     
@@ -21,11 +21,12 @@ class Admins(Base):
     username = Column(String(50), nullable=False)
     email = Column(String(100), unique=True, nullable=False, index=True)
     hashed_password = Column(String(255), nullable=False)
-    created_at = Column(DateTime, default=func.now())
-    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
+    created_at = Column(DateTime, default=func.now(), index=True)
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now(), index=True)
 
-    users_ = relationship('Users', back_populates='admin_')
+    users = relationship('Users', back_populates='admin')
 
+# Users table
 class Users(Base):
     __tablename__ = "users"
     
@@ -33,22 +34,23 @@ class Users(Base):
     username = Column(String(50), unique=True, nullable=False, index=True)
     email = Column(String(100), unique=True, nullable=False, index=True)
     hashed_password = Column(String(255), nullable=False)
-    role = Column(Enum(RoleEnum))   
-    created_at = Column(DateTime, default=func.now())
-    admin_id_ = Column(UUID(as_uuid=True), ForeignKey("admins.admins_id"), nullable=False)  # Every user is managed by an admin
-    
-    student_ = relationship('Students', back_populates='user_')
-    admin_ = relationship('Admins', back_populates='users_') 
-    teacher_ = relationship('Teachers', back_populates='user_')
-    parent_ = relationship('Parents', back_populates='user_')
-    notification_ = relationship('Notifications', back_populates='user_')
+    role = Column(Enum(RoleEnum), nullable=True)  
+    created_at = Column(DateTime, default=func.now(), index=True)
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now(), index=True)
+    admin_id = Column(UUID(as_uuid=True), ForeignKey("admins.admins_id"), nullable=False)
+
+    student = relationship('Students', back_populates='user')
+    admin = relationship('Admins', back_populates='users') 
+    teacher = relationship('Teachers', back_populates='user')
+    parent = relationship('Parents', back_populates='user')
+    notifications = relationship('Notifications', back_populates='user')
 
 # Students table
 class Students(Base):
     __tablename__ = "students"
     
     students_id = Column(UUID(as_uuid=True), default=uuid.uuid4, primary_key=True)
-    user_id_ = Column(UUID(as_uuid=True), ForeignKey("users.users_id"), nullable=False)
+    user_id = Column(UUID(as_uuid=True), ForeignKey("users.users_id"), nullable=False)
     first_name = Column(String(50), nullable=False)
     last_name = Column(String(50), nullable=False)
     dob = Column(Date, nullable=False)
@@ -56,60 +58,63 @@ class Students(Base):
     email = Column(String(100), unique=True, nullable=False, index=True)
     phone = Column(String(15), nullable=False)
     address = Column(String(255), nullable=True)
-    created_at = Column(DateTime, default=func.now())
+    created_at = Column(DateTime, default=func.now(), index=True)
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now(), index=True)
 
-    user_ = relationship('Users', back_populates='student_')
-    enrollment_ = relationship('Enrollments', back_populates='student_')
-    attendance_ = relationship('Attendance', back_populates='student_')
-    fee_ = relationship('Fees', back_populates='student_')
-    parent_ = relationship('Parents', back_populates='student_')
-    submission_ = relationship('Submissions', back_populates='student_') 
+    user = relationship('Users', back_populates='student')
+    enrollments = relationship('Enrollments', back_populates='student')
+    attendances = relationship('Attendance', back_populates='student')
+    fees = relationship('Fees', back_populates='student')
+    parents = relationship('Parents', back_populates='student')
+    submissions = relationship('Submissions', back_populates='student') 
 
 # Teachers table
 class Teachers(Base):
     __tablename__ = "teachers"
     
     teachers_id = Column(UUID(as_uuid=True), default=uuid.uuid4, primary_key=True)
-    user_id_ = Column(UUID(as_uuid=True), ForeignKey("users.users_id"), nullable=False)
+    user_id = Column(UUID(as_uuid=True), ForeignKey("users.users_id"), nullable=False)
     first_name = Column(String(50), nullable=False)
     last_name = Column(String(50), nullable=False)
     email = Column(String(100), unique=True, nullable=False, index=True)
     phone = Column(String(15), nullable=False)
-    created_at = Column(DateTime, default=func.now())
+    created_at = Column(DateTime, default=func.now(), index=True)
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now(), index=True)
 
-    user_ = relationship('Users', back_populates='teacher_')
-    class_ = relationship('Classes', back_populates='teacher_')
-    assignment_ = relationship('Assignments', back_populates='teacher_')
-    subject_ = relationship('Subjects', back_populates='teacher_')  # Ensure this line is present
+    user = relationship('Users', back_populates='teacher')
+    classes = relationship('Classes', back_populates='teacher')
+    assignments = relationship('Assignments', back_populates='teacher')
+    subjects = relationship('Subjects', back_populates='teacher')  
 
 # Parents Table
 class Parents(Base):
     __tablename__ = "parents"
     
     parents_id = Column(UUID(as_uuid=True), default=uuid.uuid4, primary_key=True)
-    student_id_ = Column(UUID(as_uuid=True), ForeignKey("students.students_id"), nullable=False)
-    user_id_ = Column(UUID(as_uuid=True), ForeignKey("users.users_id"), nullable=False)
+    student_id = Column(UUID(as_uuid=True), ForeignKey("students.students_id"), nullable=False)
+    user_id = Column(UUID(as_uuid=True), ForeignKey("users.users_id"), nullable=False)
     first_name = Column(String(50), nullable=False)
     last_name = Column(String(50), nullable=False)
     email = Column(String(100), unique=True, nullable=False, index=True)
     phone = Column(String(15), nullable=False)
     relation = Column(String(50), nullable=False)
-    created_at = Column(DateTime, default=func.now())
+    created_at = Column(DateTime, default=func.now(), index=True)
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now(), index=True)
     
-    student_ = relationship('Students', back_populates='parent_')
-    user_ = relationship('Users', back_populates='parent_')
+    student = relationship('Students', back_populates='parents')
+    user = relationship('Users', back_populates='parent')
 
 # Enrollments Table
 class Enrollments(Base):
     __tablename__ = "enrollments"
     
     enrollments_id = Column(UUID(as_uuid=True), default=uuid.uuid4, primary_key=True)
-    student_id_ = Column(UUID(as_uuid=True), ForeignKey("students.students_id"), nullable=False)
-    course_id_ = Column(UUID(as_uuid=True), ForeignKey("courses.courses_id"), nullable=False)
-    enrolled_at = Column(DateTime, default=func.now())
+    student_id = Column(UUID(as_uuid=True), ForeignKey("students.students_id"), nullable=False)
+    course_id = Column(UUID(as_uuid=True), ForeignKey("courses.courses_id"), nullable=False)
+    enrolled_at = Column(DateTime, default=func.now(), index=True)
     
-    student_ = relationship('Students', back_populates='enrollment_')
-    course_ = relationship('Courses', back_populates='enrollment_')
+    student = relationship('Students', back_populates='enrollments')
+    course = relationship('Courses', back_populates='enrollments')
 
 # Courses Table
 class Courses(Base):
@@ -118,12 +123,13 @@ class Courses(Base):
     courses_id = Column(UUID(as_uuid=True), default=uuid.uuid4, primary_key=True)
     name = Column(String(100), nullable=False)
     description = Column(Text, nullable=True)
-    created_at = Column(DateTime, default=func.now())
+    created_at = Column(DateTime, default=func.now(), index=True)
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now(), index=True)
 
-    class_ = relationship('Classes', back_populates='course_')
-    enrollment_ = relationship('Enrollments', back_populates='course_')
-    subject_ = relationship('Subjects', back_populates='course_')
-    assignment_ = relationship('Assignments', back_populates='course_')
+    classes = relationship('Classes', back_populates='course')
+    enrollments = relationship('Enrollments', back_populates='course')
+    subjects = relationship('Subjects', back_populates='course')
+    assignments = relationship('Assignments', back_populates='course')
 
 # Classes Table
 class Classes(Base):
@@ -131,14 +137,15 @@ class Classes(Base):
     
     classes_id = Column(UUID(as_uuid=True), default=uuid.uuid4, primary_key=True)
     name = Column(String(100), nullable=False)
-    teacher_id_ = Column(UUID(as_uuid=True), ForeignKey("teachers.teachers_id"), nullable=False)
-    course_id_ = Column(UUID(as_uuid=True), ForeignKey("courses.courses_id"), nullable=False)
-    created_at = Column(DateTime, default=func.now())
+    teacher_id = Column(UUID(as_uuid=True), ForeignKey("teachers.teachers_id"), nullable=False)
+    course_id = Column(UUID(as_uuid=True), ForeignKey("courses.courses_id"), nullable=False)
+    created_at = Column(DateTime, default=func.now(), index=True)
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now(), index=True)
 
-    course_ = relationship('Courses', back_populates='class_')
-    teacher_ = relationship('Teachers', back_populates='class_')
-    attendance_ = relationship('Attendance', back_populates='class_')
-    subject_ = relationship('Subjects', secondary='class_subject_link', back_populates='class_')
+    course = relationship('Courses', back_populates='classes')
+    teacher = relationship('Teachers', back_populates='classes')
+    attendance = relationship('Attendance', back_populates='class_')
+    subjects = relationship('Subjects', secondary='class_subject_link', back_populates='classes')
 
 # Subjects Table
 class Subjects(Base):
@@ -147,13 +154,14 @@ class Subjects(Base):
     subjects_id = Column(UUID(as_uuid=True), default=uuid.uuid4, primary_key=True)
     name = Column(String(100), nullable=False)
     description = Column(Text, nullable=True)
-    course_id_ = Column(UUID(as_uuid=True), ForeignKey("courses.courses_id"), nullable=False)
-    teacher_id_ = Column(UUID(as_uuid=True), ForeignKey("teachers.teachers_id"), nullable=False)  
-    created_at = Column(DateTime, default=func.now())
+    course_id = Column(UUID(as_uuid=True), ForeignKey("courses.courses_id"), nullable=False)
+    teacher_id = Column(UUID(as_uuid=True), ForeignKey("teachers.teachers_id"), nullable=False)  
+    created_at = Column(DateTime, default=func.now(), index=True)
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now(), index=True)
 
-    course_ = relationship('Courses', back_populates='subject_')
-    teacher_ = relationship('Teachers', back_populates='subject_')
-    class_ = relationship('Classes', secondary='class_subject_link', back_populates='subject_')
+    course = relationship('Courses', back_populates='subjects')
+    teacher = relationship('Teachers', back_populates='subjects')
+    classes = relationship('Classes', secondary='class_subject_link', back_populates='subjects')
 
 class ClassSubjectLink(Base):
     __tablename__ = 'class_subject_link'
@@ -165,50 +173,51 @@ class Notifications(Base):
     __tablename__ = "notifications"
     
     notifications_id = Column(UUID(as_uuid=True), default=uuid.uuid4, primary_key=True)
-    user_id_ = Column(UUID(as_uuid=True), ForeignKey("users.users_id"), nullable=False)
+    user_id = Column(UUID(as_uuid=True), ForeignKey("users.users_id"), nullable=False)
     message = Column(Text, nullable=False)
-    created_at = Column(DateTime, default=func.now())
+    created_at = Column(DateTime, default=func.now(), index=True)
 
-    user_ = relationship('Users', back_populates='notification_')
+    user = relationship('Users', back_populates='notifications')
 
 # Attendance Table
 class Attendance(Base):
     __tablename__ = "attendances"
     
     attendance_id = Column(UUID(as_uuid=True), default=uuid.uuid4, primary_key=True)
-    student_id_ = Column(UUID(as_uuid=True), ForeignKey('students.students_id'))
-    class_id_ = Column(UUID(as_uuid=True), ForeignKey('classes.classes_id'))
+    student_id = Column(UUID(as_uuid=True), ForeignKey('students.students_id'))
+    class_id = Column(UUID(as_uuid=True), ForeignKey('classes.classes_id'))
     date = Column(Date, nullable=False)
     status = Column(String(10), nullable=False)  # 'Present' or 'Absent'
     
-    student_ = relationship('Students', back_populates='attendance_')
-    class_ = relationship('Classes', back_populates='attendance_')
+    student = relationship('Students', back_populates='attendances')
+    class_ = relationship('Classes', back_populates='attendance')
 
 # Fees Table
 class Fees(Base):
     __tablename__ = "fees"
     
     fees_id = Column(UUID(as_uuid=True), default=uuid.uuid4, primary_key=True)
-    student_id_ = Column(UUID(as_uuid=True), ForeignKey('students.students_id'), nullable=False)
+    student_id = Column(UUID(as_uuid=True), ForeignKey('students.students_id'), nullable=False)
     amount = Column(Float, nullable=False)
     status = Column(String(20), nullable=False)  # 'Paid', 'Pending', etc.
     due_date = Column(Date, nullable=False)
-    created_at = Column(DateTime, default=func.now())
+    created_at = Column(DateTime, default=func.now(), index=True)
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now(), index=True)
 
-    student_ = relationship('Students', back_populates='fee_')
+    student = relationship('Students', back_populates='fees')
 
 # Submissions Table
 class Submissions(Base):
     __tablename__ = "submissions"
     
     submissions_id = Column(UUID(as_uuid=True), default=uuid.uuid4, primary_key=True)
-    student_id_ = Column(UUID(as_uuid=True), ForeignKey("students.students_id"), nullable=False)
-    assignment_id_ = Column(UUID(as_uuid=True), ForeignKey("assignments.assignments_id"), nullable=False)
-    submission_date = Column(DateTime, default=func.now())
+    student_id = Column(UUID(as_uuid=True), ForeignKey("students.students_id"), nullable=False)
+    assignment_id = Column(UUID(as_uuid=True), ForeignKey("assignments.assignments_id"), nullable=False)
+    submission_date = Column(DateTime, default=func.now(), index=True)
     grade = Column(Float, nullable=True)
 
-    student_ = relationship('Students', back_populates='submission_')
-    assignment_ = relationship('Assignments', back_populates='submission_')
+    student = relationship('Students', back_populates='submissions')
+    assignment = relationship('Assignments', back_populates='submissions')
 
 # Assignments Table
 class Assignments(Base):
@@ -217,10 +226,11 @@ class Assignments(Base):
     assignments_id = Column(UUID(as_uuid=True), default=uuid.uuid4, primary_key=True)
     title = Column(String(100), nullable=False)
     description = Column(Text, nullable=True)
-    course_id_ = Column(UUID(as_uuid=True), ForeignKey("courses.courses_id"), nullable=False)
-    teacher_id_ = Column(UUID(as_uuid=True), ForeignKey("teachers.teachers_id"), nullable=False)
-    created_at = Column(DateTime, default=func.now())
+    course_id = Column(UUID(as_uuid=True), ForeignKey("courses.courses_id"), nullable=False)
+    teacher_id = Column(UUID(as_uuid=True), ForeignKey("teachers.teachers_id"), nullable=False)
+    created_at = Column(DateTime, default=func.now(), index=True)
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now(), index=True)
 
-    teacher_ = relationship('Teachers', back_populates='assignment_')
-    course_ = relationship('Courses', back_populates='assignment_')
-    submission_ = relationship('Submissions', back_populates='assignment_')
+    teacher = relationship('Teachers', back_populates='assignments')
+    course = relationship('Courses', back_populates='assignments')
+    submissions = relationship('Submissions', back_populates='assignment')
