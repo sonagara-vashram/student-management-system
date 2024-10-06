8761f740569c47a031ea9756f37339f27a10197f
sonagara-vashram <vashramsonagara0106@gmail.com>
2024-10-06 22:04:28 +0530

    Complete student file

--- app/__student.py
+++ app/__student.py
@@ -1,7 +1,8 @@
 from typing import Annotated
+from uuid import UUID
 from sqlalchemy.orm import Session
 from fastapi import FastAPI, Depends, HTTPException, Path
-from models import Students, Users
+from models import RoleEnum, Students, Users
 from starlette import status
 from database import SessionLocal
 from schema import StudentRequest
@@ -24,30 +25,33 @@ async def read_all(db: db_dependency):
 @app.post("/student", status_code=status.HTTP_201_CREATED)
 async def create_student(db: db_dependency, std_request: StudentRequest):
     user = db.query(Users).filter(Users.users_id == std_request.user_id).first()
-    if not user:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid User ID.")
+    if user is None or user.role != RoleEnum.STUDENT:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Invalid user ID. User must be a student.')
     
-    role = std_request.role
-    
-    # db.add(std_model)
-    # db.commit()
-    # db.refresh(std_model)
-    # return std_model
+    existing_student = db.query(Students).filter(Students.user_id == std_request.user_id).first()
+    if existing_student:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Student already exists for this user.")
+
+    std_model = Students(**std_request.model_dump())
+    db.add(std_model)
+    db.commit()
+    db.refresh(std_model)
+    return std_model
     
 @app.get("/student/{student_id}", status_code=status.HTTP_200_OK)
-async def read_students(db: db_dependency, student_id: int = Path(gt=0)):
+async def read_students(db: db_dependency, student_id: UUID):
     std_model = db.query(Students).filter(Students.students_id == student_id).first()
     if std_model is not None:
         return std_model
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Student not found.')
 
 @app.put("/student/{student_id}")
-async def update_student(db: db_dependency, std_request: StudentRequest, student_id: int = Path(gt=0)):
+async def update_student(db: db_dependency, std_request: StudentRequest, student_id: UUID):
     std_model = db.query(Students).filter(Students.students_id == student_id).first()
     if std_model is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Student not found.')
 
-    std_model.user_id_ = std_request.user_id_
+    std_model.user_id = std_request.user_id
     std_model.first_name = std_request.first_name
     std_model.last_name = std_request.last_name
     std_model.dob = std_request.dob
@@ -62,7 +66,7 @@ async def update_student(db: db_dependency, std_request: StudentRequest, student
     return std_model
     
 @app.delete("/student/{student_id}", status_code=status.HTTP_200_OK)
-async def delete_student(db: db_dependency, student_id: int = Path(gt=0)):
+async def delete_student(db: db_dependency, student_id: UUID):
     std_model = db.query(Students).filter(Students.students_id == student_id).first()
     if std_model is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Student not found.')
